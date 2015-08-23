3532c989a0c72af7b39970e00828911426a7a1d1
Manu <bionikspoon@gmail.com>
2015-08-23 18:52:07 -0500

    active url template tag

---
+++ pubmed/templatetags/annotation_tool_tags.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+import re
+
+from django import template
+from django.core.urlresolvers import reverse, NoReverseMatch
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def active_url(context, label, url, *args, **kwargs):
+    template_string = ('<li {li_attrs}>'
+                       '<a href="{url}">'
+                       '{label}'
+                       '</a>'
+                       '</li>')
+    try:
+        url = reverse(url, args=args, kwargs=kwargs)
+        pattern = '^%s$' % url
+    except NoReverseMatch:
+        pattern = url
+
+    path = context['request'].path
+    li_attrs = "class='active'" if re.search(pattern, path) else ''
+    return template_string.format(li_attrs=li_attrs, url=url, label=label)
