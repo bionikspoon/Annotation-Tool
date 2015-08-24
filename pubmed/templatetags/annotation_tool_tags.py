d31a30ab2005c3eb17dfa9c7b9a4946f76137c56
Manu <bionikspoon@gmail.com>
2015-08-23 19:00:51 -0500

    il18n template tag

--- pubmed/templatetags/annotation_tool_tags.py
+++ pubmed/templatetags/annotation_tool_tags.py
@@ -4,6 +4,7 @@ import re
 
 from django import template
 from django.core.urlresolvers import reverse, NoReverseMatch
+from django.utils.translation import ugettext as _
 
 register = template.Library()
 
@@ -23,4 +24,4 @@ def active_url(context, label, url, *args, **kwargs):
 
     path = context['request'].path
     li_attrs = "class='active'" if re.search(pattern, path) else ''
-    return template_string.format(li_attrs=li_attrs, url=url, label=label)
+    return template_string.format(li_attrs=li_attrs, url=url, label=_(label))
