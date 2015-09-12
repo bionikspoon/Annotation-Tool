5faf8a31f668f4ed82e1090ef18634458fe558a0
Manu <bionikspoon@gmail.com>
2015-09-12 02:09:05 -0500

    fix compressor

--- core/utils/compressor.py
+++ core/utils/compressor.py
@@ -7,7 +7,7 @@ class CachedS3BotoStorage(S3BotoStorage):
         super().__init__(*args, **kwargs)
         self.local_storage = get_storage_class('compressor.storage.CompressorFileStorage')()
 
-    def save(self, name, content, max_length=None):
-        name = super(CachedS3BotoStorage, self).save(name, content, max_length=max_length)
-        self.local_storage._save(name, content, max_length=max_length)
+    def save(self, *args, **kwargs):
+        name = super(CachedS3BotoStorage, self).save(*args, **kwargs)
+        self.local_storage._save(*args, **kwargs)
         return name
