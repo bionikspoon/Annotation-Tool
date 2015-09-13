f76aad2d36743267e79e1a7949cb388f8eba0926
Manu <bionikspoon@gmail.com>
2015-09-12 23:51:20 -0500

    fixed disappearing generated_data

--- pubmed/tests/test_factories.py
+++ pubmed/tests/test_factories.py
@@ -2,6 +2,7 @@ import logging
 from django.test import TestCase
 
 from ..factories import EntryFactory
+from pubmed.lookups import DiseaseLookup, StructureLookup
 
 logger = logging.getLogger(__name__)
 
@@ -16,9 +17,12 @@ class TestEntryFactory(TestCase):
         entry2 = EntryFactory()
 
         self.assertNotEqual(entry1, entry2)
+        self.assertTrue(entry1.structure)
         self.assertNotEqual(entry1.pubmed_id, entry2.pubmed_id)
 
     def test_factories_populate_many_to_many_fields(self):
-        # entry = EntryFactory(disease=(22,))
+        # self.assertEqual(StructureLookup.objects.count(), 10)
+        self.assertEqual(DiseaseLookup.objects.count(), 10)
+        entry = EntryFactory(disease=(22,))
         # self.assertTrue(len(entry.disease) > 1)
         pass
