cac6a5c7d2f72d3db976e4f70ac7359c11c9e1ce
Manu <bionikspoon@gmail.com>
2015-09-13 19:38:37 -0500

    refactored utils

--- annotation_tool/contrib/sites/migrations/0002_set_site_domain_and_name.py
+++ annotation_tool/contrib/sites/migrations/0002_set_site_domain_and_name.py
@@ -5,12 +5,10 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 # Local Application
-from ..utils import update_site_backward, update_site_forward
+from . import update_site_backward, update_site_forward
 
 
 class Migration(migrations.Migration):
     dependencies = [('sites', '0001_initial'), ]
 
-    operations = [migrations.RunPython(update_site_forward, update_site_backward),
-
-                  ]
+    operations = [migrations.RunPython(update_site_forward, update_site_backward)]
