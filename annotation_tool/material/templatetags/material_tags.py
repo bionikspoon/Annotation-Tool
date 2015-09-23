9ad1f19efd456865ab26cb0a1bf954dbfd97e832
Manu Phatak <bionikspoon@gmail.com>
2015-09-23 16:00:30 -0500

    floppy forms

--- annotation_tool/material/templatetags/material_tags.py
+++ annotation_tool/material/templatetags/material_tags.py
@@ -1,4 +1,15 @@
 from django import template
+# from django.forms import widgets
+from floppyforms import widgets
 
 register = template.Library()
 
+
+@register.filter
+def is_input(field):
+    return isinstance(field.field.widget, widgets.TextInput)
+
+
+@register.filter
+def is_textarea(field):
+    return isinstance(field.field.widget, widgets.Textarea)
