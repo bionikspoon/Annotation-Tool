6084c3135443e158504054831147398969a64e0f
Manu Phatak <bionikspoon@gmail.com>
2015-10-05 15:03:26 -0500

    add limit to gene list view api

--- server/annotation_tool/pubmed/views.py
+++ server/annotation_tool/pubmed/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import (Pubmed, StructureLookup, MutationTypeLookup, SyntaxLookup, RuleLevelLookup, VariantTypeLookup,
                      VariantConsequenceLookup, DiseaseLookup, PatientOutcomesLookup, Gene)
@@ -11,6 +12,18 @@ from .serializers import (PubmedSerializer, DiseaseSerializer, VariantConsequenc
 class GeneViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Gene.objects.all()
     serializer_class = GeneSerializer
+
+    def list(self, request, *args, **kwargs):
+        # TODO remove limit, add pagination
+        queryset = self.filter_queryset(self.get_queryset())[:100]
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class StructureViewSet(viewsets.ModelViewSet):
