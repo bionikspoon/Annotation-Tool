ed85e8ab97fdfbb2306f38f1f4a460c7595d2866
Manu Phatak <bionikspoon@gmail.com>
2015-10-05 12:36:21 -0500

    pubmed_data documentation

--- server/annotation_tool/pubmed/management/commands/pubmed_data.py
+++ server/annotation_tool/pubmed/management/commands/pubmed_data.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""Build and load initial pubmed data."""
+
 import re
 from itertools import chain
 from pathlib import Path
@@ -10,61 +12,12 @@ from django.core.management.base import BaseCommand
 from django.core.serializers.base import DeserializationError
 
 
-class config:
+class Config:
+    """Command constants."""
     _CWD = Path(__file__, '..').resolve()
     DATA_DIR = (_CWD / '_pubmed_data').resolve()
     FIXTURES_DIR = (_CWD / '../..' / "fixtures").resolve()
     APP = 'pubmed'
-
-
-def parse_choices(file):
-    with file.open() as f:
-        data = f.read().splitlines()
-
-    # split by newline and ";",  filter empty, flatten
-    data = set(chain(*[re.split(r'; ?', line) for line in data if line]))
-
-    # clean data. replace "_" and title case
-    data = map(lambda x: x.replace('_', ' ').title(), data)
-
-    # remove obscure symbols
-    data = map(lambda x: re.sub(r'[^\w\s\-\(\)]', '', x), data)
-
-    # clean whitespace. run generator
-    data = (re.sub(r' +', ' ', item.strip()) for item in data if item)
-
-    return set(data)
-
-
-def build_fixtures(choices, model, app=config.APP):
-    fixture_name = '%s.%s' % (app, model)
-    return [dict(model=fixture_name, pk=pk, fields={'choice': choice}) for pk, choice in
-            enumerate(sorted(choices), start=1)]
-
-
-def save_fixture_to_file(data, name):
-    file = config.FIXTURES_DIR.joinpath('%s.yaml' % name)
-    with file.open('w') as f:
-        yaml.dump(data, f)
-
-    print('Fixture generated:', file)
-
-
-def build_from_source():
-    for file in config.DATA_DIR.glob('*.txt'):
-        choices = parse_choices(file)
-        fixtures = build_fixtures(choices, file.stem)
-        save_fixture_to_file(fixtures, file.stem)
-
-
-def load_fixtures():
-    for file in config.FIXTURES_DIR.glob('*.yaml'):
-        try:
-            call_command('loaddata', file.name)
-        except (KeyError, LookupError, DeserializationError):
-            import sys
-
-            print('Model %s.%s not found. Import skipped' % (config.APP, file.stem), file=sys.stdout)
 
 
 class Command(BaseCommand):
@@ -72,22 +25,107 @@ class Command(BaseCommand):
     help = __doc__
 
     def add_arguments(self, parser):
-        """:type parser: django.core.management.base.CommandParser"""
+        """
+        Command arguments.
+
+        :param django.core.management.base.CommandParser parser: Parse arguments.
+        """
         parser.add_argument('--build-only', '-b', default=False, action='store_true',
                             help="Only build from source. Skip import.")
         parser.add_argument('--import-only', '-i', default=False, action='store_true',
                             help='Only import existing fixtures.  Skip building from source.')
 
     def handle(self, *args, **options):
+        """
+        Command handle.
+
+        :param args:
+        :param options: Argument map from `add_arguments`.
+        """
         build_only, import_only = options.get('build_onl'), options.get('import_only')
 
         if build_only:
-            build_from_source()
+            self.build_from_source()
             return
 
         if import_only:
-            load_fixtures()
+            self.load_fixtures()
             return
 
-        build_from_source()
-        load_fixtures()
+        self.build_from_source()
+        self.load_fixtures()
+
+    @staticmethod
+    def parse_choices(file):
+        """
+        Get set of choices from raw data file.
+
+        :param pathlib.Path file: Path object, referencing file.
+        :return: Set of choices.
+        :rtype: set
+        """
+        with file.open() as f:
+            data = f.read().splitlines()
+
+        # split by newline and ";",  filter empty, flatten
+        data = set(chain(*[re.split(r'; ?', line) for line in data if line]))
+
+        # clean data. replace "_" and title case
+        data = map(lambda x: x.replace('_', ' ').title(), data)
+
+        # remove obscure symbols
+        data = map(lambda x: re.sub(r'[^\w\s\-\(\)]', '', x), data)
+
+        # clean whitespace. run generator
+        data = (re.sub(r' +', ' ', item.strip()) for item in data if item)
+
+        return set(data)
+
+    @staticmethod
+    def build_fixtures(choices, model, app=Config.APP):
+        """
+        Build fixtures from choices.
+
+        :param set choices: Set of choices.
+        :param str model: Model to be referenced in fixture.
+        :param str app: Django app to be referenced in fixture.
+        :return: List of fixtures.
+        :rtype: [dict]
+        """
+        fixture_name = '%s.%s' % (app, model)
+        return [dict(model=fixture_name, pk=pk, fields={'choice': choice}) for pk, choice in
+                enumerate(sorted(choices), start=1)]
+
+    @staticmethod
+    def dump(fixtures, name, filepath=Config.FIXTURES_DIR):
+        """
+        Dump fixtures to `.yaml` file.
+
+        :param [dict] fixtures: List of fixtures.
+        :param name: Name for file output.
+        :param pathlib.Path filepath: Path object pointing to fixtures directory.
+        """
+        file = filepath.joinpath('%s.yaml' % name)
+        with file.open('w') as f:
+            yaml.dump(fixtures, f)
+
+        print('Fixture generated:', file)
+
+    def build_from_source(self):
+        """Build process.  Get files -> get choices -> build fixtures -> dump yaml."""
+        for file in Config.DATA_DIR.glob('*.txt'):
+            choices = self.parse_choices(file)
+            fixtures = self.build_fixtures(choices, file.stem)
+            self.dump(fixtures, file.stem)
+
+    @staticmethod
+    def load_fixtures():
+        """Save fixtures into db."""
+
+        for file in Config.FIXTURES_DIR.glob('*.yaml'):
+            try:
+                call_command('loaddata', file.name)
+            except (KeyError, LookupError, DeserializationError):
+                import sys
+
+                print('Model %s.%s not found. Import skipped' % (Config.APP, file.stem), file=sys.stdout)
