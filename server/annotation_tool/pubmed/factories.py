6c3ffc3982392e1f4b3344d31a9b4fa439554884
Manu Phatak <bionikspoon@gmail.com>
2015-10-08 22:21:41 -0500

    fixed fake data

--- server/annotation_tool/pubmed/factories.py
+++ server/annotation_tool/pubmed/factories.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 from random import randint, choice
-
 from factory import DjangoModelFactory, Iterator, LazyAttribute
 from faker import Faker
-
 from .models import (StructureLookup, MutationTypeLookup, SyntaxLookup, RuleLevelLookup, VariantTypeLookup,
                      PatientOutcomesLookup, Choices, VariantConsequenceLookup, DiseaseLookup, Pubmed, Gene)
 from ..users.models import User
@@ -30,15 +28,15 @@ class PubmedFactory(DjangoModelFactory):
     mutation_type = Iterator(MutationTypeLookup.objects.all())
     syntax = Iterator(SyntaxLookup.objects.all())
     syntax_text = make(faker.text, max_nb_chars=128)
-    operator = Iterator(Choices.OPERATOR)
+    operator = Iterator(choice[0] for choice in Choices.OPERATOR)
     rule_level = Iterator(RuleLevelLookup.objects.all())
     chromosome = Iterator(choice[0] for choice in Choices.RANGE_TWENTY_THREE)
     chromosome_range = LazyAttribute(lambda _: (randint(12, 12), randint(12, 24)))
-    breakend_strand = Iterator(Choices.STRAND)
-    breakend_direction = Iterator(Choices.DIRECTION)
+    breakend_strand = Iterator(choice[0] for choice in Choices.STRAND)
+    breakend_direction = Iterator(choice[0] for choice in Choices.DIRECTION)
     mate_chromosome = Iterator(choice[0] for choice in Choices.RANGE_TWENTY_THREE)
     mate_chromosome_range = LazyAttribute(lambda _: (randint(12, 12), randint(12, 24)))
-    mate_breakend_strand = Iterator(Choices.STRAND)
+    mate_breakend_strand = Iterator(choice[0] for choice in Choices.STRAND)
     number_of_copies = LazyAttribute(lambda _: (randint(12, 12), randint(12, 24)))
     coordinate_predicate = make(faker.text, max_nb_chars=32)
     partner_coordinate_predicate = make(faker.text, max_nb_chars=32)
@@ -48,7 +46,7 @@ class PubmedFactory(DjangoModelFactory):
     disease = many_to_many(DiseaseLookup, 'disease')
     treatment_number_of_arms = Iterator(choice[0] for choice in Choices.RANGE_FIVE)
     population_size = make(faker.random_int)
-    sex = Iterator(Choices.SEX)
+    sex = Iterator(choice[0] for choice in Choices.SEX)
     ethnicity = make(faker.text, max_nb_chars=128)
     assessed_patient_outcomes = many_to_many(PatientOutcomesLookup, 'assessed_patient_outcomes')
     significant_patient_outcomes = many_to_many(PatientOutcomesLookup, 'significant_patient_outcomes')
